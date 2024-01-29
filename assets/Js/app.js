d42ce851134a96ce0dbe3fdbcd6f6aff82c7331e
narozbekjon <jamilovnavroz32@gmail.com>
2024-01-29 11:30:00 +0500

    hgwehjhdejhke

---
+++ assets/Js/app.js
@@ -0,0 +1,37 @@
+alert("Salom foydalanuvchi siz Jamilov Navro'zbekning siteiga kirmoqchimisiz")
+prompt("Iltimos foydalanuvchi ismingizni va familyaizni kiriting")
+prompt("Iltimos foydalanuvchi yoshingizni kiriting")
+prompt("Iltimos foydalanuvchi shaxsingizni kiriting")
+prompt("Iltimos foydalanuvchi qaysi oyda tug'ulganingizni kiriting")
+prompt("Iltimos foydalanuvchi qaysi sanada tug'ulganingizni kiriting")
+confirm("Tabriklaymiz foydalanuvchi siz Jamilov Navro'zbekning siteiga muaffaqqiyatli tarizda kirdingiz")
+const number1 = 1;
+const number2 = 2;
+const number3 = 3;
+const number4 = 4;
+const number5 = 5;  
+const number6 = 6;  
+const number7 = 7;  
+const number8 = 8;  
+const number9 = 9;  
+const number10 = 10;
+var number11 = 11;
+var number12 = 12;
+var number13 = 13;
+var number14 = 14;
+var number15 = 15;  
+var number16 = 16;  
+var number17 = 17;  
+var number18 = 18;  
+var number19 = 19;  
+var number20 = 20;
+let number21 = 21;
+let number22 = 22;
+let number23 = 23;
+let number24 = 24;
+let number25 = 25;  
+let number26 = 26;  
+let number27 = 27;  
+let number28 = 28;  
+let number29 = 29;  
+let number30 = 30;            
